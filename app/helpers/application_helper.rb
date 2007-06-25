8e781ec23a000889febffa078077c8261a625e33
Alex <alex@gradientblue.com>
2007-06-25 19:33:06 +0000

    Ticket #14, corrected typo in spelling of helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     </script>"
   end
   
-  def offiste_link(title,url)
+  def offsite_link(title,url)
     url = "http://" + url unless url.include? "http://" or url.include? "https://"
     "<a href=\"/site/exit?dest=#{url}\" class=\"offsite\">
       #{title}
