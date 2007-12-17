ebee0a37d50c02489523267eedd24d53c383c951
Daniel Piltch <dan@freeportmetrics.com>
2007-12-17 05:21:44 +0000

    fix to ordering of news items when displayed

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,12 +2,12 @@ class NewsController < ApplicationController
   
   def index
     @scroll = true
-    @internal_news = News.find(:all, :conditions => ["is_internal = 1", "publish_date < now()", "archive_date > now()"], :order => "publish_date desc", :limit => 3)
-    @external_news = News.find(:all, :conditions => ["is_internal = 0", "publish_date < now()", "archive_date > now()"], :order => "publish_date desc", :limit => 3)
+    @internal_news = News.find(:all, :conditions => ["is_internal = 1", "publish_date < now()", "archive_date > now()"], :order => "position asc", :limit => 3)
+    @external_news = News.find(:all, :conditions => ["is_internal = 0", "publish_date < now()", "archive_date > now()"], :order => "position asc", :limit => 3)
   end
   
   def external_news
     @scroll = true
-    @external_news = News.find(:all, :conditions => ["is_internal = 0", "publish_date < now()", "archive_date > now()"], :order => "publish_date desc")
+    @external_news = News.find(:all, :conditions => ["is_internal = 0", "publish_date < now()", "archive_date > now()"], :order => "position asc")
   end
 end
