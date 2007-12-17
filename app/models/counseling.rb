e1134827b51f3eee5c2c3b1f44a94196ba8eb2cf
Stuart <stuart@gradientblue.com>
2007-12-17 18:07:04 +0000

    added method to check for aoa coverage

--- app/models/counseling.rb
+++ app/models/counseling.rb
@@ -12,4 +12,15 @@ class Counseling < ActiveRecord::Base
   belongs_to :hq_state, :class_name => "State", :foreign_key => "hq_state_abbrev"
   belongs_to :pension_state, :class_name => "State", :foreign_key => "pension_state_abbrev"
   
+  def has_aoa_coverage?
+    sql = <<-SQL
+        select 1 from agencies a 
+        join locations l on l.agency_id = a.id 
+        join restrictions r on r.location_id = l.id or r.agency_id = a.id 
+        join restrictions_states rs on rs.restriction_id = r.id 
+        where a.result_type_id = ? and rs.state_abbrev = ?
+        SQL
+    Agency.find_by_sql([sql, ResultType['AoA'], state_abbrev]).size > 0
+  end
+  
 end
