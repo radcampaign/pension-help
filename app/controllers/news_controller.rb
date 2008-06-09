cfa9335f8692b201089d171e70b8a5a6497ca7a7
Daniel Piltch <dan@freeportmetrics.com>
2008-06-09 19:28:14 +0000

    #245 link to more news items.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,13 +1,13 @@
 class NewsController < ApplicationController  
   
   def index
-    @scroll = true
-    @internal_news = News.find(:all, :conditions => "is_internal = 1 AND publish_date <= current_date() AND archive_date > current_date()", :order => "position asc, publish_date desc", :limit => 3)
-    @external_news = News.find(:all, :conditions => "is_internal = 0 AND publish_date <= current_date() AND archive_date > current_date()", :order => "position asc, publish_date desc", :limit => 3)
+    # @scroll = true
+    @internal_news = News.find(:all, :conditions => "is_internal = 1 AND publish_date <= current_date() AND archive_date > current_date()", :order => "position asc, publish_date desc")
+    @external_news = News.find(:all, :conditions => "is_internal = 0 AND publish_date <= current_date() AND archive_date > current_date()", :order => "position asc, publish_date desc")
   end
   
   def external_news
-    @scroll = true
+    # @scroll = true
     @external_news = News.find(:all, :conditions => "is_internal = 0 AND publish_date <= current_date() AND archive_date > current_date()", :order => "position asc, publish_date desc")
   end
 end
