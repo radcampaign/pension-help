760673eb0a7c7a5df3e2e22a8515f6161ee2a5ae
Daniel Piltch <dan@freeportmetrics.com>
2008-06-09 19:36:06 +0000

    #237 - add blank selection to employee types, and add full internal_news page

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -10,4 +10,10 @@ class NewsController < ApplicationController
     # @scroll = true
     @external_news = News.find(:all, :conditions => "is_internal = 0 AND publish_date <= current_date() AND archive_date > current_date()", :order => "position asc, publish_date desc")
   end
+
+  def internal_news
+    # @scroll = true
+    @external_news = News.find(:all, :conditions => "is_internal = 1 AND publish_date <= current_date() AND archive_date > current_date()", :order => "position asc, publish_date desc")
+  end
+
 end
