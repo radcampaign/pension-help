47caeb8ffb23f1e2ba7fdd794cd5cb63deb60160
Kamil <kamil@freeportmetrics.com>
2009-10-27 20:05:28 +0000

    #349

--- app/helpers/partners_helper.rb
+++ app/helpers/partners_helper.rb
@@ -6,6 +6,27 @@ module PartnersHelper
   
   def hourly_rate_display
     @partner.hourly_rate_str || number_to_currency(@partner.hourly_rate.to_s, :precision => 2)
-  end  
+  end
+
+  def sponsor_type_class_display(item)
+    html_class= ''
+    html_class << (item.use_for_pal ? 'use_for_pal ' : '')  
+    html_class << (item.use_for_search ? 'use_for_search ' : '')
+    html_class << (item.use_for_npln ? 'use_for_npln ' : '')
+    html_class << (item.use_for_help ? 'use_for_help' : '')
+    html_class.strip
+  end
+  
+  def plan_type_class_display(item)    
+   if item.use_for_pal
+     'use_for_pal use_for_npln use_for_help'
+   else
+     'use_for_npln use_for_help'
+   end
+  end
+
+  def refferals_class_display(item)
+     plan_type_class_display(item) 
+  end
   
 end
