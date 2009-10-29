cfc5b792ebd33404274cb8539b52e337f59934e3
Kamil <kamil@freeportmetrics.com>
2009-10-29 12:07:23 +0000

    #349

--- app/helpers/partners_helper.rb
+++ app/helpers/partners_helper.rb
@@ -13,15 +13,16 @@ module PartnersHelper
     html_class << (item.use_for_pal ? 'use_for_pal ' : '')  
     html_class << (item.use_for_search ? 'use_for_search ' : '')
     html_class << (item.use_for_npln ? 'use_for_npln ' : '')
-    html_class << (item.use_for_help ? 'use_for_help' : '')
+    html_class << (item.use_for_help ? 'use_for_help ' : '')
+    html_class << 'remove_on_submit'
     html_class.strip
   end
   
   def plan_type_class_display(item)    
    if item.use_for_pal
-     'use_for_pal use_for_npln use_for_help'
+     'use_for_pal use_for_npln use_for_help remove_on_submit'
    else
-     'use_for_npln use_for_help'
+     'use_for_npln use_for_help remove_on_submit'
    end
   end
 
