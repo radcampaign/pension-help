6e5a1f49d730cd90cdd19d0e308b2b30ae6d0a11
Daniel Piltch <dan@freeportmetrics.com>
2009-12-17 05:27:15 +0000

    enable tinyMCE in safari

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -7,7 +7,7 @@ class Admin::ContentController < ApplicationController
   end
 
   uses_tiny_mce(:options => {:theme => 'advanced',
-                             :browsers => %w{msie gecko},
+                             :browsers => %w{msie gecko safari opera},
                              :theme_advanced_toolbar_location => "top",
                              :theme_advanced_toolbar_align => "left",
                              :paste_auto_cleanup_on_paste => true,
@@ -21,7 +21,7 @@ class Admin::ContentController < ApplicationController
                              :theme_advanced_buttons2 => [],
                              :theme_advanced_buttons3 => [],
                              :forced_root_block => 'p',
-                             :plugins => %w{contextmenu paste}},
+                             :plugins => %w{contextmenu paste safari}},
                 :only => [:new, :edit])
   
   
--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -7,7 +7,7 @@ class Admin::NewsController < ApplicationController
   end
 
   uses_tiny_mce(:options => {:theme => 'advanced',
-                             :browsers => %w{msie gecko},
+                             :browsers => %w{msie gecko safari opera},
                              :theme_advanced_toolbar_location => "top",
                              :theme_advanced_toolbar_align => "left",
                              :paste_auto_cleanup_on_paste => true,
@@ -19,8 +19,8 @@ class Admin::NewsController < ApplicationController
                              :theme_advanced_buttons1 => %w{bold italic underline strikethrough separator justifyleft justifycenter justifyright separator indent outdent separator bullist numlist separator undo redo separator link unlink separator styleselect formatselect separator code},
                              :theme_advanced_buttons2 => [],
                              :theme_advanced_buttons3 => [],
-                             :plugins => %w{contextmenu paste}},
-                             :extended_valid_elements => %w{a[name|href|target|title|onclick] img[class|src|border=0|alt|title|hspace|vspace|width|height|align|name] hr[class|width|size|noshade] font[face|size|color|style] span[class|align|style]},
+                             :plugins => %w{contextmenu paste safari},
+                             :extended_valid_elements => %w{a[name|href|target|title|onclick] img[class|src|border=0|alt|title|hspace|vspace|width|height|align|name] hr[class|width|size|noshade] font[face|size|color|style] span[class|align|style]}},
                 :only => [:new, :edit])
   
   def index
