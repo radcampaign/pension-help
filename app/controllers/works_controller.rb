61c905b218a34b4e5834b6ce2e3176617cef7582
Marcin Walczak <marcin@freeportmetrics.com>
2011-02-08 09:29:49 +0100

    few gems added gems added

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -19,11 +19,11 @@ class WorksController < ApplicationController
         session[:partner] = @partner
         self.current_user = User.authenticate(user[:login], user[:password])
 
-        #Mailer.deliver_help_net_application(@partner) if @partner.wants_help
-        #Mailer.deliver_search_net_application(@partner) if @partner.wants_search
-        #Mailer.deliver_npln_application(@partner) if @partner.wants_npln
-        #Mailer.deliver_aaa_application(@partner) if @partner.wants_pal
-        #Mailer.deliver_user_application_confirmation(@partner) # sends confirmation email to user
+        Mailer.deliver_help_net_application(@partner) if @partner.wants_help
+        Mailer.deliver_search_net_application(@partner) if @partner.wants_search
+        Mailer.deliver_npln_application(@partner) if @partner.wants_npln
+        Mailer.deliver_aaa_application(@partner) if @partner.wants_pal
+        Mailer.deliver_user_application_confirmation(@partner) # sends confirmation email to user
 
         redirect_to edit_partner_path(self.current_user.partner)
       end
