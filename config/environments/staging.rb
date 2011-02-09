41328d36ffb53b79adb78c3a164ef7f4ad564185
Marcin Walczak <marcin@freeportmetrics.com>
2011-02-09 18:24:04 +0100

    fixed staging; removed dump

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,9 +1,20 @@
 config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
-ActionMailer::Base.smtp_settings = { 
-  :address => 'localhost',
-  :domain  => 'pensionhelp.org'
+require 'tlsmail'
+
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.gmail.com',
+  :domain  => 'gmail.com',
+  :port => 587,
+  :authentication => :plain,
+  :user_name => 'pensionhelpamerica@gmail.com',
+  :password => 'gblue2011',
+  :tls =>  true
+
 }
-EMAIL_RECIPIENT = "JHotz@PensionRights.org, dan@gradientblue.com"
+
+EMAIL_RECIPIENT = "dan@freeportmetrics.com, marcin@freeportmetrics.com"
 EMAIL_FROM = "do-not-reply@PensionRights.org"
