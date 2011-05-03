800e9002336a36417e9c9bacbbea63132122be3a
Daniel Piltch <dan@blackboard.local>
2011-05-03 12:16:37 -0400

    get rid of "news" functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,17 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :passwords
   map.resources :restrictions
   map.resources :users, :has_one => [:password]
-  
+
   map.resources :agencies do |agency|
     agency.resources :locations
     agency.resources :plans
   end
-  
+
   map.resources :feedbacks,
       :controller => :feedback
 
   # The priority is based upon order of creation: first created -> highest priority.
-  
+
   # Sample of regular route:
   # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
@@ -22,18 +22,17 @@ ActionController::Routing::Routes.draw do |map|
   # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # You can have the root of your site routed by hooking up '' 
+  # You can have the root of your site routed by hooking up ''
   # -- just remember to delete public/index.html.
   # map.connect '', :controller => "welcome"
 
   # Install the default route as the lowest priority.
   #route to: behind scenes search
-  map.connect 'internal_news/:id/show', :controller => 'news', :action => 'internal'
   map.connect 'search2', :controller => "search", :action => "search2"
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action.:format'
   map.connect ':controller/:action/:id'
-    
+
   # Site URLs
   map.with_options(:controller => 'site') do |site|
     site.homepage          '',              :action => 'show_page', :url => '/'
