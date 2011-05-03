b64cb38535504a58edc43d94986f743aa869ee23
Daniel Piltch <dan@blackboard.local>
2011-05-03 10:07:20 -0400

    don't track development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,18 +23,14 @@ require 'tlsmail'
 Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 config.action_mailer.raise_delivery_errors = true
 
-ActionMailer::Base.smtp_settings = { 
-  :address => 'smtp.gmail.com',
-  :domain  => 'gmail.com',
-  :port => 587,
-  :authentication => :plain,
-  :user_name => 'pensionhelpamerica@gmail.com',
-  :password => 'gblue2011',
-  :tls =>  true
-  
+ActionMailer::Base.smtp_settings = {
+  :address => 'mail.pensionrights.org',
+  :domain  => 'pensionrights.org',
+  :port => 25,
+  :authentication => :none
 }
 
-  
+
   EMAIL_RECIPIENT = "dan@gradientblue.com, dan@piltch.com"
   EMAIL_FROM = "dan@gradientblue.com"
 
