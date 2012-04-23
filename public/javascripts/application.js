c0940810d3a68b42066fbe4221a41f0b36272cd4
Filip Tepper <filip@tepper.pl>
2012-04-23 15:45:39 +0200

    formatting cleanup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,38 +1,37 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
-// for zebra striping tables. 
+// for zebra striping tables.
 // from http://blog.internautdesign.com/2007/11/30/zebra-striping-with-prototype
+Event.observe(window, 'DOMContentLoaded', function() {
 
-Event.observe( window, 'DOMContentLoaded', function() {
-
-  $$('.zebra').each( function(e) {
-    Selector.findChildElements(e, ['tr','li']).each( function(e, idx) {
-      e.addClassName( idx % 2 == 1 ? 'odd' : 'even');
-    });    
+  $$('.zebra').each(function(e) {
+    Selector.findChildElements(e, ['tr', 'li']).each(function(e, idx) {
+      e.addClassName(idx % 2 == 1 ? 'odd' : 'even');
+    });
   });
 
-(function($) {
-  $.fn.increaseFontsize = function(size, speed, easing, callback) {
-    return this.animate({fontSize: size}, speed, easing, callback);
-  };
-})(jQuery);
+  (function($) {
+    $.fn.increaseFontsize = function(size, speed, easing, callback) {
+      return this.animate({
+        fontSize: size
+      }, speed, easing, callback);
+    };
+  })(jQuery);
 
+  alert("aaaa")
 
-
-jQuery(function() {
-  jQuery('.font').click(function() {
-    if (jQuery(this).attr('class')=="small font") {
+  jQuery(function() {
+    jQuery('.font').click(function() {
+      if (jQuery(this).attr('class') == "small font") {
         jQuery('body, p, div, td, tr,label').increaseFontsize(10, 'fast');
-    }
-    if (jQuery(this).attr('class')=="medium font") {
+      }
+      if (jQuery(this).attr('class') == "medium font") {
         jQuery('body, p, div, td, tr,label').increaseFontsize(13, 'fast');
-    }
-     if (jQuery(this).attr('class')=="big font") {
+      }
+      if (jQuery(this).attr('class') == "big font") {
         jQuery('body, p, div, td, tr,label').increaseFontsize(17, 'fast');
-    }
+      }
+    });
   });
-});
 
 });
-
