9277c43790ae8dcca00c978bcd5fa5015e0f3122
Daniel Piltch <dan@freeportmetrics.com>
2012-04-24 15:50:44 -0400

    refs #5416 - update emails for link checker

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,6 +3,8 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 require 'tlsmail'
 
+config.action_mailer.default_url_options = { :host => "qa.pensionhelp.org" }
+
 Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 
 ActionMailer::Base.smtp_settings = {
@@ -16,7 +18,7 @@ ActionMailer::Base.smtp_settings = {
 
 }
 
-EMAIL_RECIPIENT = "dan@freeportmetrics.com, jhotz@pensionrights.org, JLeavelle@PensionRights.org"
+EMAIL_RECIPIENT = "dan@gradientblue.com"
 EMAIL_FROM = "do-not-reply@PensionRights.org"
-LINK_CHECKER_RECIPIENT = "dan@freeportmetrics.com, jhotz@pensionrights.org, JLeavelle@PensionRights.org"
+LINK_CHECKER_RECIPIENT = "dan@gradientblue.com, JLeavelle@PensionRights.org, KGarrett@PensionRights.org"
 LINK_CHECKER_FROM = "do-not-reply@PensionRights.org"
