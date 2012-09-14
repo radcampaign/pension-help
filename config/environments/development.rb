56c4db1c54c8d962da46502107a4cd38fa8af78f
Daniel Piltch <dan@freeportmetrics.com>
2012-09-14 17:00:35 -0400

    refs #7902 - fix layout of results page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,8 +43,8 @@ ActionMailer::Base.smtp_settings = {
 }
 
 
-  EMAIL_RECIPIENT = "dan@gradientblue.com"
-  EMAIL_FROM = "dan@gradientblue.com"
-  LINK_CHECKER_RECIPIENT = "dan@gradientblue.com"
-  LINK_CHECKER_FROM = "dan@gradientblue.com"
+  EMAIL_RECIPIENT = "dan@freeportmetrics.com"
+  EMAIL_FROM = "dan@freeportmetrics.com"
+  LINK_CHECKER_RECIPIENT = "dan@freeportmetrics.com"
+  LINK_CHECKER_FROM = "dan@freeportmetrics.com"
 
