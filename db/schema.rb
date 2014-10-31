e170f6588574bea9e292723d2bb4491a3326f608
Jakub Piasecki <jakub@freeportmetrics.com>
2014-10-31 17:18:05 +0100

    refs #14869 - Try prototype rails plugin - gemfile cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030102754) do
+ActiveRecord::Schema.define(version: 20141029154928) do
 
   create_table "addresses", force: true do |t|
     t.integer "location_id"
@@ -952,12 +952,10 @@ ActiveRecord::Schema.define(version: 20141030102754) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "zip_import", id: false, force: true do |t|
     t.string  "zipcode",      limit: 5,                          null: false
