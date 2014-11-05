11e0f4d8806e057780fc33b8922b7be09cb3146b
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2014-11-05 10:41:11 +0100

    refs #14806 Update Gems: http_authentication - apply fix for configurable parameters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :http_authentication_required
+  before_action :configure_permitted_parameters, if: :devise_controller?
   http_basic_authenticate_with name: "pha", password: "ph2012a", if: :http_authentication_required , only: [:new]
 
   protected
