c7ff1362efdebfae777598bcebc6d2ee0cb4d295
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2014-11-07 09:56:05 +0100

    refs #14810 Update Gems: tiny_mce - code cleanup

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -4,19 +4,7 @@ class Admin::ContentController < ApplicationController
   def authorized?
     current_user.is_admin?
   end
-  #
-  # tinymce(:options => {:theme => 'advanced',
-  #                            :browsers => %w{msie gecko safari opera},
-  #                            :theme_advanced_styles => 'Offsite Link=offsite',
-  #                            :theme_advanced_buttons1 => %w{bold italic underline strikethrough separator justifyleft justifycenter justifyright separator indent outdent separator bullist numlist separator undo redo separator link unlink anchor separator styleselect formatselect separator code ts_image},
-  #                           :extended_valid_elements => %w{a[name|href|target|title|onclick|class|id] img[class|src|border=0|alt|title|hspace|vspace|width|height|align|name|usemap] hr[class|width|size|noshade] font[face|size|color|style] span[class|align|style] map[id|name] area[shape|coords|href|alt|target]},
-  #                            :theme_advanced_buttons2 => [],
-  #                            :theme_advanced_buttons3 => [],
-  #                            :forced_root_block => 'p',
-  #                            :plugins => %w{contextmenu paste safari}},
-  #               :only => [:new, :edit])
 
-  
   def index
     list
     render :action => 'list'
