a5ff01d3a5ef3305783e8e3ecfb34aedb726d69c
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2014-11-14 16:46:54 +0100

    refs #14974 Deployment: Commit and push to Engine Yard. do a little  - update staging configuration, and staging data

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,8 +1,10 @@
+Rails.application.configure do
+
 config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
-config.action_mailer.default_url_options = { :host => "qa.pensionhelp.org" }
+config.action_mailer.default_url_options = { :host => "ec2-107-20-224-223.compute-1.amazonaws.com" }
 
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.gmail.com',
@@ -14,10 +16,12 @@ ActionMailer::Base.smtp_settings = {
     :password => 'pha:1021'
 }
 
-EMAIL_RECIPIENT = "dan@freeportmetrics.com"
-EMAIL_FROM = "webmaster@pensionhelp.org"
-LINK_CHECKER_RECIPIENT = "dan@freeportmetrics.com"
-LINK_CHECKER_FROM = "webmaster@pensionhelp.org"
+EMAIL_RECIPIENT = "jakub@freeportmetrics.com"
+EMAIL_FROM = "jakub@freeportmetrics.com"
+LINK_CHECKER_RECIPIENT = "jakub@freeportmetrics.com"
+LINK_CHECKER_FROM = "jakub@freeportmetrics.com"
 
-GA_ACCOUNT = 'UA-39402011-1'
-GA_EXPERIMENTS = '70321422-0'
+GA_ACCOUNT = '123TEST'
+GA_EXPERIMENTS = '123TEST'
+
+end
