ad8bfc51c488964cdf775352f4191cade12a1f33
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2014-11-17 13:11:04 +0100

    refs #14974 Deployment: Commit and push to Engine Yard. do a little  - trying to check devise secretbase

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,9 +5,9 @@ Devise.setup do |config|
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   # config.secret_key = 'd6dec9cc34ac0fae0c3e842b3530e92fc488b79ad5f9cc8694c0d3eccdc6bd4090a38258620f039e5d33ef2f0dbd9e1284d8b6e2eea0876878a241278c0e5d34'
-  if Rails.env.production? || Rails.env.staging?
-    config.secret_key = ENV['SECRET_KEY_BASE']
-  end
+  # if Rails.env.production? || Rails.env.staging?
+  #   config.secret_key = ENV['DEVISE_SECRET_KEY']
+  # end
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
