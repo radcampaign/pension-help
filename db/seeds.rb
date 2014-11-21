2f77fc9c3d9851ec8bc48c79717df9f76d8e3e0a
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2014-11-21 12:01:05 +0100

    refs #14974 Deployment: Commit and push to Engine Yard. - create seed for QA server

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env == 'staging'
+  u = User.new(login: 'qa_test', email: 'alicja@freeportmetrics.com', password: 'freeport2014', password_confirmation: 'freeport2014')
+  u.roles.push(Role.where(role_name: "ADMIN"))
+  u.save!
+end
+
