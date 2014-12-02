5a99fad06a62eae22d4f6aeaa2b04eb1faf4e5f3
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2014-12-02 12:00:10 +0100

    refs #14808 Update Gems: session_timeout - change session timeout from 10 days to 4 hours

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pension-help_session', :expire_after => 10.days
+Rails.application.config.session_store :cookie_store, key: '_pension-help_session', :expire_after => 4.hours
